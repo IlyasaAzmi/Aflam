45e346d77a6e0579471cd206860e272bb7942552
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-24 07:53:29 +0700

    Initial Commit

---
+++ Aflam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aflam
+//
+//  Created by Ilyasa Azmi on 24/03/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
